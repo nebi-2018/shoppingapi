8aeb843bf96304ff318ad4f67bede28af9bc9b70
Nebi <mam10122011@yahoo.com>
2023-03-02 19:05:52 +0200

    just some logs

--- products/src/index.ts
+++ products/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
-  console.log("Satrting up products2...");
+  console.log("Satrting up products3...");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
