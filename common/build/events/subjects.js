c8d874d1f89d827ffe27fafa4a15e0e61abd7047
Nebi <mam10122011@yahoo.com>
2023-03-15 14:44:53 +0200

    Updates

--- common/build/events/subjects.js
+++ common/build/events/subjects.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Subjects = void 0;
 var Subjects;
 (function (Subjects) {
+    Subjects["UserCreated"] = "user:created";
     Subjects["ProductCreated"] = "product:created";
     Subjects["ProductUpdated"] = "product:updated";
     Subjects["OrderCreated"] = "order:created";
