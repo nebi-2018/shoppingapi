8d05a7bba79915c75ce2b890a9b649209c579cd2
Nebi <mam10122011@yahoo.com>
2023-03-18 20:13:39 +0200

    Updates

--- common/src/events/user-created-event.ts
+++ common/src/events/user-created-event.ts
@@ -3,6 +3,7 @@ import { Subjects } from "./subjects";
 export interface UserCreatedEvent {
   subject: Subjects.UserCreated;
   data: {
+    id: string;
     fullName: string;
     email: string;
     stripeCustomerId: string;
