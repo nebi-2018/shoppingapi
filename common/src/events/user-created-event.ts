34c25e04a1aeea65f8ad7f6b4af7ab6a66dee2a2
Nebi <mam10122011@yahoo.com>
2023-03-18 18:06:21 +0200

    Updates

--- common/src/events/user-created-event.ts
+++ common/src/events/user-created-event.ts
@@ -3,6 +3,7 @@ import { Subjects } from "./subjects";
 export interface UserCreatedEvent {
   subject: Subjects.UserCreated;
   data: {
+    id: string;
     userId: string;
     fullName: string;
     email: string;
